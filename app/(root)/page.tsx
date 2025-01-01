b3ed632d2965a07c084d2c73a9c85c4c23c73d91
Gilad Weinberger <giladwe21@gmail.com>
2025-01-01 20:19:02 +0200

    wnat to check i environment_variables updated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
+import { auth } from "@/auth";
 import Navbar from "@/components/Navbar";
 
-export default function Home() {
-  
+export default async function Home() {
+  const session = await auth();
+  const is_authenticated = Boolean(session?.user);
+  const user = session?.user;
+
   return (
     <>
       <Navbar />
